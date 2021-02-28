524ad47b7de01e43514b0c96d0b151e1402b26e6
NABENDU <nabendubose1991@gmail.com>
2021-03-01 00:56:21 +0530

    This is a backend application for react-crud-application

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customer =Customer::orderBy('id','desc')->get();
+        return response()->json($customer);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer;
+        $customer->email = $request->email;
+        $customer->password = $request->password;
+        $customer->save();
+        return response()->json($customer);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customer = Customer::findOrFail($id);
+        return response()->json($customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->email = $request->email;
+        $customer->password = $request->password;
+        $customer->save();
+        return response()->json($customer);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+        $customer->delete();
+
+    //     $article = Customer::findOrFail($id);
+    // if($article)
+    //    $article->delete(); 
+    // else
+    //     return response()->json(error);
+    // return response()->json(null); 
+        
+
+    }
+}
